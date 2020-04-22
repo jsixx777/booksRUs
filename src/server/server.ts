8e4d33da5a70f611795f2491a53fd0ecf5f27cce
JPSiler <jsixx77@hotmail.com>
2020-04-21 20:43:17 -0500

    adding bareBones

---
+++ src/server/server.ts
@@ -0,0 +1,24 @@
+import * as path from 'path';
+import * as express from 'express';
+import routes from './routes';
+import * as passport from 'passport';
+import './middleware/localstrategy';
+import './middleware/bearerstrategy';
+
+const app = express();
+
+let p = path.join(__dirname, '../public');
+
+
+app.use(express.static(p));
+app.use(express.json());
+app.use(passport.initialize());
+app.use(routes);
+app.get("*", (req,res)=> {
+    res.sendFile(path.join(__dirname, "../public/index.html"))
+})
+
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+    console.log(`Server listening on port: ${port}`);
+});
